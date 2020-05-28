c079e027a3285968024449bc0f4b150f14edb3c5
Cara Zellner <carajeanine@gmail.com>
2020-05-28 22:23:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(string)
-  if string is == "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
